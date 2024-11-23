c1cbe92e95a02435cb99469f1a1b0e10ebce4505
Hamad <2020cs152@student.uet.edu.pk>
2024-11-23 11:10:07 +0500

    new key

--- utils/stipe.js
+++ utils/stipe.js
@@ -2,6 +2,6 @@
 "use client";
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe('pk_live_4NvvktNi6W8LjRam8qMrCY86'); // Replace with your Stripe publishable key
+const stripePromise = loadStripe('pk_test_51PgM3ERrXZMuJmTGxuePJkqFCbXYgnV35djshusb2zYOQlydY7LqxBMVZJxYJ9zyCEOooyjo91mKyzf6TUERzvoz001jFYTYq6'); // Replace with your Stripe publishable key
 
 export default stripePromise;
